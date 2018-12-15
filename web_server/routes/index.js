d1f2cf83be619401d9dd4258d4bd682f89c4c1a2
Yue Wu <yuriyuewu@gmail.com>
2018-12-14 22:08:11 -0500

    Create signup and login page in jade

--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Go for Real Estate' });
 });
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Go for Real Estate' });
+});
+
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Go for Real Estate' });
+});
+
+router.post('/signup', function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+})
 
 module.exports = router;
